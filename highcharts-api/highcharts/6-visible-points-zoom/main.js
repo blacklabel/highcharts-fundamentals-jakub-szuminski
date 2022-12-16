41a8a6bad7d419b59cf09a84e154685eaf560594
Jakub Szumiński <jakubszuminskixrp@gmail.com>
2022-12-16 14:58:05 +0100

    Resolved issues after feedback

--- highcharts-api/highcharts/6-visible-points-zoom/main.js
+++ highcharts-api/highcharts/6-visible-points-zoom/main.js
@@ -4,81 +4,41 @@ Highcharts.chart('container', {
     chart: {
         zoomType: 'xy',
         events: {
-            load() {
+            render() {
                 const chart = this,
                     yAxis = chart.yAxis[0],
-                    maxY = yAxis.dataMax;
-                
-                chart.maxCircles = [];
-                
-                for(let point of yAxis.series[0].data) {
-                    if(point.y !== maxY) continue;
-
-                    chart.maxCircles.push(
-                        [chart.renderer.circle(chart.xAxis[0].toPixels(point.x), yAxis.toPixels(0), 5)
-                        .attr({ zIndex: 5, })
-                        .css({ color: 'red' })
-                        .add(),
-                        point.x]
-                    );
-                }
-
-                chart.visibleCount = chart.renderer.text(
-                    `Visible count: ${yAxis.series[0].data.length}`, 
-                    chart.plotLeft, 
-                    chart.plotTop + chart.plotHeight + 50
-                ).add();
-
-                chart.update({
-                    plotOptions: {
-                        series: {
-                            dataLabels: {
-                                formatter() {
-                                    return this.point.y === maxY ? this.point.y : '';
-                                }
-                            }
-                        }
-                    }
-                });
-            }, 
-            redraw() {
-                const chart = this,
-                    yAxis = chart.yAxis[0];
-                
-                const yMin = yAxis.min,
+                    maxY = yAxis.dataMax,
+                    yMin = yAxis.min,
                     yMax = yAxis.max,
                     xMin = chart.xAxis[0].min,
                     xMax = chart.xAxis[0].max;
+                    
                 
-                let visibleCount = 0;
+                if(chart.maxCircles) chart.maxCircles.forEach(circle => circle.destroy());
+                chart.maxCircles = [];
 
-                const checkPoint = (point) => (
-                    (yMin <= point.y && point.y <= yMax) && (xMin <= point.x && point.x <= xMax)
-                );
-                
                 for(let point of yAxis.series[0].data) {
-                    if(checkPoint(point)) visibleCount++;
+                    if(point.y !== maxY) continue;
+
+                    const circle = chart.renderer.circle(chart.xAxis[0].toPixels(point.x), chart.plotTop + chart.plotHeight, 5)
+                        .attr({ zIndex: 5, })
+                        .css({ color: 'red' })
+                        .add();
+                    chart.maxCircles.push(circle);
                 }
 
-                for(let circle of chart.maxCircles) {
-                    circle[0].attr({
-                        x: chart.xAxis[0].toPixels(circle[1])
-                    });
+                let visibleCount = 0;
+                for(let point of yAxis.series[0].data) {
+                    if(yMin <= point.y && point.y <= yMax && xMin <= point.x && point.x <= xMax)
+                        visibleCount++;
                 }
 
-                if(chart.visibleCount) {
-                    chart.visibleCount.attr({
-                        text: `Visible count: ${visibleCount}`,
-                        x: chart.plotLeft,
-                        y: chart.plotTop + chart.plotHeight + 50,
-                    })
-                } else {
-                    chart.visibleCount = chart.renderer.text(
-                        `Visible count: ${visibleCount}`, 
-                        chart.plotLeft, 
-                        chart.plotTop + chart.plotHeight + 50
-                    ).add();
-                }
+                if(!chart.visibilityInfoText) chart.visibilityInfoText = chart.renderer.text().add();
+                chart.visibilityInfoText.attr({
+                    text: `Visible count: ${visibleCount}`,
+                    x: chart.plotLeft,
+                    y: chart.plotTop + chart.plotHeight + 50
+                });
             }
         }
     },
@@ -91,6 +51,9 @@ Highcharts.chart('container', {
                     color: 'red',
                     textOutline: 'white',
                 },
+                formatter() {
+                    return this.point.y === this.point.series.dataMax ? this.point.y : '';
+                }
             }
         }
     },
