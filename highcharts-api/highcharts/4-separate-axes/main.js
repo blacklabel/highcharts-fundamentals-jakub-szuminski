cde08b0eb2b4b99582c97d5c6783798256c76e48
Jakub Szumiński <jakubszuminskixrp@gmail.com>
2022-12-16 14:27:16 +0100

    Resolved issues after code review

--- highcharts-api/highcharts/4-separate-axes/main.js
+++ highcharts-api/highcharts/4-separate-axes/main.js
@@ -6,7 +6,6 @@ Highcharts.chart("container", {
         marginTop: 50,
         marginLeft: 20,
         marginRight: 20,
-
         events: {
             load() {
                 const leftAxis = this.yAxis[0],
@@ -26,105 +25,83 @@ Highcharts.chart("container", {
             },
         }
     },
-    
     title: {
-        text: "",
+        text: ""
     },
-
     plotOptions: {
-        series: {
-            stacking: 'normal',
+        bar: {
+            stacking: 'overlap'
         }
     },
-
     xAxis: {
         categories: categories,
-        
         title: {
             text: "",
         },
-
         lineWidth: 0,
-        
         labels: {
             align: 'left',
         },
-
         left: '50%',
-        width: '20%',  
+        width: '20%'  
     },
-
     legend: {
-        enabled: false,
+        enabled: false
     },
-
     yAxis: [
         {
             max: 100,
             width: '40%',
-            
             reversed: true,
-
             title: {
                 text: 'Non managerial position',
             },
-
             backgroundColor: 'gray',
         },
         {
             max: 100,
             width: '40%',
-            
             offset: 0,
             left: '60%',
-
             title: {
                 text: 'Managerial position',
             }
         },
     ],
-
     series: [
         {
-            data: [50, 36, 50, 40, 14],
+            data: [100, 100, 100, 100, 100], 
             color: 'gray',
             yAxis: 0,
-            enableMouseTracking: false,
+            enableMouseTracking: false
         },
         {
             data: [50, 64, 50, 60, 86],
-
             color: "red",
             yAxis: 0,
-
             dataLabels: {
                 enabled: true,
                 inside: 'true',
                 align: "right",
-                
-                format: '{y}%',
-            },
+                format: '{y}%'
+            }
         },
-
         {
-            data: [32, 43, 55, 78, 12],
+            data: [100, 100, 100, 100, 100],
             color: 'gray',
             yAxis: 1,
             enableMouseTracking: false
         },
         {
             data: [68, 57, 45, 22, 88],
-            
             color: "red",
             yAxis: 1,
-
             dataLabels: {
                 enabled: true,
                 inside: 'true',
                 align: "left",
-                
-                format: '{y}%',
-            },
-        },
-    ],
+                format: '{y}%'
+            }
+        }
+    ]
 });
