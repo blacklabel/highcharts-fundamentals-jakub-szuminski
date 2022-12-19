353c9dbf279d0fa7813441a4b03c13bacd21dd0d
Jakub Szumiński <jakubszuminskixrp@gmail.com>
2022-12-19 10:48:19 +0100

    Created first version of the solution.

---
+++ highcharts-api/highcharts/11-click-mousemove-cursor/main.js
@@ -0,0 +1,29 @@
+const container = document.getElementById('container');
+
+const createCircle = (x = 0, y = 0) => {
+    return chart.renderer
+        .circle(x, y, 5)
+        .attr({ 'zIndex': 5, 'stroke': 'black', 'fill': 'blue' })
+        .add();
+}
+
+container.addEventListener('mousemove', (e) => {
+    if(!chart.cursorPointer) 
+        chart.cursorPointer = createCircle();
+
+    chart.cursorPointer.attr({
+        x: e.x,
+        y: e.y
+    });
+});
+
+container.addEventListener('click', (e) => {
+    if(chart.cursorPointer) 
+        chart.cursorPointer = createCircle(e.x, e.y);
+});
+
+const chart = Highcharts.chart('container', {
+    series: [{
+        data: [45, 75, 110, 125, 148, 175, 135, 150, 225, 180, 80, 55],
+    }]
+});
