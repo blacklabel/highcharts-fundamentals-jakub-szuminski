8f2be6a249cd8fb156b06d1ee26ac3f833d092de
Jakub Szumiński <jakubszuminskixrp@gmail.com>
2022-12-21 11:13:47 +0100

    a

--- highcharts-api/highcharts/15-heatmap-hsv/main.js
+++ highcharts-api/highcharts/15-heatmap-hsv/main.js
@@ -19,15 +19,37 @@ function hsvToRgb(hue, saturation, value) {
     return `rgba(${R}, ${G}, ${B})`;
 }
 
+function hslToHex(h, s, l) {
+    const a = s * Math.min(l, 1 - l);
+    const f = n => {
+      const k = (n + h / 30) % 12;
+      const color = l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
+      return Math.round(255 * color).toString(16).padStart(2, '0');   // convert to Hex and prefix "0" if needed
+    };
+
+    const result = `#${f(0)}${f(8)}${f(4)}`;
+    console.log('Conversion result: ', result);
+
+    return `#${f(0)}${f(8)}${f(4)}`;
+}
+
 let arr = [];
 
+/*
 for(let x = 0; x < 360; x += 10) {
     for(let y = 0; y < 1; y += 0.025) {
-        arr.push([x, y, hsvToRgb(x, y, 1)]);
+        arr.push([x, y, x * y]);
+        //arr.push([x, y, hsvToRgb(x, y, 1)]);
+    }
+}*/
+
+for(let x = 0; x < 360; x += 1) {
+    for(let y = 0; y < 1; y += 0.1) {
+        arr.push([x, y, x * y]);
     }
 }
 
-console.log(arr);
+//console.log(arr);
 
 
 Highcharts.chart('container', {
@@ -37,6 +59,30 @@ Highcharts.chart('container', {
     
     title: {
         text: 'Heatmap of colors in HSV'
+    },
+
+    plotOptions: {
+        series: {
+            turboThreshold: 10000
+        }
+    },
+
+    colorAxis: {
+        min: 0,
+        max: 10,
+        
+    },
+
+    yAxis: {
+        min: 0,
+        max: 1,
+        tickInterval: 0.1,
+    },
+
+    xAxis: {
+        min: 0,
+        max: 360,
+        tickInterval: 60,
     },
 
     //step y: 0.05
