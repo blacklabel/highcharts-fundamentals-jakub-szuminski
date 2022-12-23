ad3bc51ace3bd8748961d84210c8c17e4ee8ff73
Jakub Szumiński <jakubszuminskixrp@gmail.com>
2022-12-23 09:47:08 +0100

    Added buttons to control DataGrouping.

--- highcharts-api/highcharts-stock/1-live-data-tools/main.js
+++ highcharts-api/highcharts-stock/1-live-data-tools/main.js
@@ -11,7 +11,6 @@ function generateData() {
         data.push([i, Math.floor(Math.random() * 100)]);
     }
 
-    console.log(data);
     return data;
 }
 
@@ -19,8 +18,8 @@ Highcharts.stockChart('chart-one', {
     series: [{
         name: 'AAPL',
         data: generateData(),
-        tooltip: {
-            valueDecimals: 2
+        dataGrouping: {
+            groupPixelWidth: 5,
         }
     }]
 });
@@ -29,8 +28,68 @@ Highcharts.stockChart('chart-two', {
     series: [{
         name: 'AAPL',
         data: generateData(),
-        tooltip: {
-            valueDecimals: 2
+    }],
+
+    stockTools: {
+        gui: {
+            iconsURL: './',
+            buttons: ['increaseDataGrouping', 'decreaseDataGrouping'],
+            definitions: {
+                increaseDataGrouping: {
+                    className: 'increase-data-grouping',
+                    symbol: 'arrow-up.svg'
+                },
+                decreaseDataGrouping: {
+                    className: 'decrease-data-grouping',
+                    symbol: 'arrow-down.svg'
+                }
+            }
+        },
+    },
+
+    navigation: {
+        bindings: {
+            increaseDataGrouping: {
+                className: 'increase-data-grouping',
+                init: function(e) {
+                    console.log(this);
+                    console.log(e);
+
+                    console.log('increasing...');
+
+                    const series = this.chart.series[0],
+                        groupPixelWidth = series.groupPixelWidth;
+
+                    if(groupPixelWidth >= 30) {
+                        alert('Maximum value of DataGrouping achieved');
+                    }
+
+                    series.update({
+                        dataGrouping: {
+                            groupPixelWidth: groupPixelWidth + 2
+                        }
+                    });
+                }
+            },
+            decreaseDataGrouping: {
+                className: 'decrease-data-grouping',
+                init: function(e) {
+                    console.log('decreasing...');
+
+                    const series = this.chart.series[0],
+                        groupPixelWidth = series.groupPixelWidth;
+
+                    if(groupPixelWidth <= 1) {
+                        alert('Minimum value of DataGrouping achieved');
+                    }
+
+                    series.update({
+                        dataGrouping: {
+                            groupPixelWidth: groupPixelWidth - 2
+                        }
+                    });
+                }
+            }
         }
-    }]
+    }
 });
