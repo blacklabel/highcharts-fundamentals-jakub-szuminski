fef88f5ea5facd99edf611c17ad342b877cb6fa6
Jakub Szumiński <jakubszuminskixrp@gmail.com>
2022-12-27 09:06:43 +0100

    Created first version of the solution.

--- highcharts-api/highcharts-maps/1-points-joinby/main.js
+++ highcharts-api/highcharts-maps/1-points-joinby/main.js
@@ -1,14 +1,30 @@
-Highcharts.mapChart('container', {
+//Note: there's an error in README (mapbble)
 
-    series: [{
-        data: [
-            ['POL', 100],
-            ['USA', 90],
-            ['PER', 50],
-            ['TZA', 40],
-            ['AUS', 1]
-        ],
-        joinBy: ['iso-a3']
-    }]
+Highcharts.getJSON('https://code.highcharts.com/mapdata/custom/world.geo.json', function (geojson) {
+    Highcharts.mapChart('container', {
+        chart: {
+            map: geojson,
+        },
 
+        series: [{
+            data: [
+                ['POL', 100],
+                ['USA', 80],
+                ['PER', 50],
+                ['TZA', 40],
+                ['AUS', 1],
+            ],
+            joinBy: 'iso-a3',
+            keys: ['iso-a3', 'value'],
+        }, {
+            type: 'mapbubble',
+            joinBy: ['iso-a3', 'code3'],
+            data: [
+                { code3: 'POL', z: 42000, code: 'POL' },
+                { code3: 'USA', z: 38000, code: 'USA' },
+                { code3: 'AFG', z: 34656, code: 'AF' },
+                { code3: 'AUS', z: 3210, code: 'AUS' },
+            ],
+        }]
+    });
 });
