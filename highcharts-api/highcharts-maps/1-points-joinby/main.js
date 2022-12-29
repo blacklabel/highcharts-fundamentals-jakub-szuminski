bab864d416c253611bc23b24b2397fe5ee7f9b0e
Jakub Szumiński <jakubszuminskixrp@gmail.com>
2022-12-29 11:35:08 +0100

    Resolved issues after first code review. Removed unnecessary codes & applied different method importing chart.map

--- highcharts-api/highcharts-maps/1-points-joinby/main.js
+++ highcharts-api/highcharts-maps/1-points-joinby/main.js
@@ -1,30 +1,25 @@
-//Note: there's an error in README (mapbble)
-
-Highcharts.getJSON('https://code.highcharts.com/mapdata/custom/world.geo.json', function (geojson) {
-    Highcharts.mapChart('container', {
-        chart: {
-            map: geojson,
-        },
-
-        series: [{
-            data: [
-                ['POL', 100],
-                ['USA', 80],
-                ['PER', 50],
-                ['TZA', 40],
-                ['AUS', 1],
-            ],
-            joinBy: 'iso-a3',
-            keys: ['iso-a3', 'value'],
-        }, {
-            type: 'mapbubble',
-            joinBy: ['iso-a3', 'code3'],
-            data: [
-                { code3: 'POL', z: 42000, code: 'POL' },
-                { code3: 'USA', z: 38000, code: 'USA' },
-                { code3: 'AFG', z: 34656, code: 'AF' },
-                { code3: 'AUS', z: 3210, code: 'AUS' },
-            ],
-        }]
-    });
+Highcharts.mapChart('container', {
+    chart: {
+        map: 'custom/world',
+    },
+    series: [{
+        data: [
+            ['POL', 100],
+            ['USA', 80],
+            ['PER', 50],
+            ['TZA', 40],
+            ['AUS', 1],
+        ],
+        joinBy: 'iso-a3',
+        keys: ['iso-a3', 'value']
+    }, {
+        type: 'mapbubble',
+        joinBy: ['iso-a3', 'code3'],
+        data: [
+            { code3: 'POL', z: 42000 },
+            { code3: 'USA', z: 38000 },
+            { code3: 'AFG', z: 34656 },
+            { code3: 'AUS', z: 3210 }
+        ],
+    }]
 });
